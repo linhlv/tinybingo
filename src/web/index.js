c9ad57a2ed8f48c58bd1644748ba9ad530ac86c9
Linh Le <linhle@smoovapp.com>
2017-12-19 15:59:52 +0700

    Simple API/Web

--- src/web/index.js
+++ src/web/index.js
@@ -1,9 +1,5 @@
 module.exports = (app) =>{
     app.get('/', (req, res, next)=>{
         return res.render('index.ejs', {});
-    });    
-
-    app.get('/login', (req, res, next)=>{
-        return res.render('login.ejs', {});
-    });    
+    });   
 }
